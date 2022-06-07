67a4ab60b4a38ed6fd761db6475d2d2eb04792a5
Ruiqi Huang <kiyoakila@gmail.com>
2022-06-07 15:37:18 -0700

    insersion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -14,12 +14,26 @@
 */
 
 function insertionSort(nums) {
-  // code goes here
+  // i - index the sorted list
+  for (let i = 0; i < nums.length - 1; i++) {
+    // j - index the insersion item
+    for (let j = i + 1; j < nums.length; j++) {
+      // insert the item to the currect position
+      while (nums[j] < nums[j - 1] && j > 0) {
+        // swap
+        const temp = nums[j];
+        nums[j] = nums[j - 1];
+        nums[j - 1] = temp;
+        j--;
+      }
+    }
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test.skip('insertion sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
