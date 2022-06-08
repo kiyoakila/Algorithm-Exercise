b379d57841423cc89c16cc234e941e0ed6f35596
Ruiqi Huang <kiyoakila@gmail.com>
2022-06-07 20:43:53 -0700

    merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,30 @@
   You'll need to write more than just one function
 */
 
-const mergeSort = (nums) => {
-  // code goes here
+const mergeSort = (A) => {
+  // base case
+  if (A.length == 1) {
+    return A;
+  } else {
+    // break and merge
+    const m = Math.floor(A.length / 2);
+    const L = mergeSort(A.slice(0, m));
+    const R = mergeSort(A.slice(m));
+    return merge(L, R);
+  }
+  function merge(A, B) {
+    const C = [];
+    while (A.length > 0 && B.length > 0) {
+      // shift the smaller number to C
+      C.push(A[0] < B[0] ? A.shift() : B.shift());
+    }
+    return C.concat(0 == A.length ? B : A);
+  }
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test.skip('merge sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
